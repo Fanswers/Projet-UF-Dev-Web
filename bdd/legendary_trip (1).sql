ef727f499b21142f68a379a9504fa524f14fe609
Jucrz <jujucruz99@outlook.fr>
2020-06-16 12:26:01 +0200

    Ajout de la bdd

--- bdd/legendary_trip (1).sql
+++ bdd/legendary_trip (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 16 juin 2020 à 11:39
+-- Généré le : mar. 16 juin 2020 à 12:25
 -- Version du serveur :  10.4.11-MariaDB
 -- Version de PHP : 7.4.4
 
@@ -44,16 +44,19 @@ CREATE TABLE `logement` (
   `proprietaire_id` int(6) DEFAULT NULL,
   `latitude` float DEFAULT NULL,
   `longitude` float DEFAULT NULL,
-  `departement` int(3) DEFAULT NULL
+  `image` varchar(100) DEFAULT NULL,
+  `image_text` text DEFAULT NULL,
+  `departement` int(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `logement`
 --
 
-INSERT INTO `logement` (`id_logement`, `logement_type`, `total_occupant`, `total_chambre`, `total_toilette`, `adresse`, `tv`, `kitchen`, `climatisation`, `chauffage`, `internet`, `prix`, `date_publication`, `proprietaire_id`, `latitude`, `longitude`, `departement`) VALUES
-(10, 'maison', 2, 2, 2, 'La maison', 1, 1, 1, 1, 0, 200, NULL, 7, NULL, NULL, 11),
-(11, 'maison', 5, 4, 1, '1 impasse du petit bois', 0, 0, 0, 1, 1, 200, NULL, 7, NULL, NULL, 24);
+INSERT INTO `logement` (`id_logement`, `logement_type`, `total_occupant`, `total_chambre`, `total_toilette`, `adresse`, `tv`, `kitchen`, `climatisation`, `chauffage`, `internet`, `prix`, `date_publication`, `proprietaire_id`, `latitude`, `longitude`, `image`, `image_text`, `departement`) VALUES
+(22, 'Villa vue sur la mer', 8, 4, 3, '37 route de la mer', 1, 1, 1, 1, 1, 1200, NULL, 6, NULL, NULL, 'VacancesLuxe.jpg', 'Villa toute équipée avec vue imprenable sur la mer, longue piscine extérieur, climatisation dans toutes les pièces plus internet très haut débit disponible. Barbecue au gaz déjà alimenté, four, micro-onde et lave vaisselle dans la cuisine.  ', 33),
+(24, 'Villa piscine + anne', 6, 3, 2, '48 rue Bernard Adour', 1, 1, 1, 1, 1, 900, NULL, 8, NULL, NULL, 'VillaNum2.jpg', 'Villa toute équipée, longue piscine extérieur, deuxième salon mi-extérieur dans un annexe climatisation dans toutes les pièces plus internet très haut débit disponible. Barbecue au gaz déjà alimenté, four, micro-onde et lave vaisselle dans la cuisine.  ', 6),
+(25, 'Villa moderne ', 10, 5, 3, '37 chemin de Mauvarre', 1, 1, 1, 1, 1, 1100, NULL, 8, NULL, NULL, 'villaNum3.jpg', 'Villa moderne toute équipée, longue piscine extérieur, climatisation dans toutes les pièces plus internet très haut débit disponible. Barbecue au gaz déjà alimenté, four, micro-onde et lave vaisselle dans la cuisine.  ', 6);
 
 -- --------------------------------------------------------
 
@@ -71,15 +74,6 @@ CREATE TABLE `reservation` (
   `nb_personne` int(2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Déchargement des données de la table `reservation`
---
-
-INSERT INTO `reservation` (`id_reservation`, `id_utilisateur`, `id_logement`, `date_debut`, `dete_fin`, `prix_total`, `nb_personne`) VALUES
-(2, 7, 10, '2020-08-10', '2020-08-17', 1000, 2),
-(5, 7, 10, '2020-08-10', '2020-08-10', 100, 3),
-(6, 7, 10, '2020-06-02', '2020-06-24', 100, 2);
-
 -- --------------------------------------------------------
 
 --
@@ -93,9 +87,9 @@ CREATE TABLE `utilisateur` (
   `pseudo` varchar(20) DEFAULT NULL,
   `motDePasse` varchar(30) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
-  `numero` int(10) NOT NULL,
-  `code_postal` int(5) DEFAULT NULL,
-  `adresse` varchar(100) DEFAULT NULL,
+  `numero` varchar(20) NOT NULL,
+  `code_postal` int(11) NOT NULL,
+  `adresse` varchar(100) NOT NULL,
   `sold` int(8) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -104,8 +98,8 @@ CREATE TABLE `utilisateur` (
 --
 
 INSERT INTO `utilisateur` (`id_utilisateur`, `prenom`, `nom`, `pseudo`, `motDePasse`, `email`, `numero`, `code_postal`, `adresse`, `sold`) VALUES
-(7, 'Patrick', 'Petit', 'Fanswers', 'P@ssword1234', 'exemple@gmail.com', 612345678, 42000, '69 rue des exemples', 48),
-(8, 'test', 'test', 'test', 'P@ssword1234', 'test@test.com', 123456789, 12345, 'test', 0);
+(6, 'Alexis', 'Larrode', 'Fanswers', 'P@ssword1234', 'alexis.larrode@gmail.com', '0', 0, '', 0),
+(8, 'Julien', 'Cruz', 'Jucrz', 'P@ssword1234', 'julien.cruz@ynov.com', '06 06 06 06 06', 78000, 'El V&eacute;sizou', 0);
 
 --
 -- Index pour les tables déchargées
@@ -116,12 +110,6 @@ INSERT INTO `utilisateur` (`id_utilisateur`, `prenom`, `nom`, `pseudo`, `motDePa
 --
 ALTER TABLE `logement`
   ADD PRIMARY KEY (`id_logement`);
-
---
--- Index pour la table `reservation`
---
-ALTER TABLE `reservation`
-  ADD PRIMARY KEY (`id_reservation`);
 
 --
 -- Index pour la table `utilisateur`
@@ -137,19 +125,13 @@ ALTER TABLE `utilisateur`
 -- AUTO_INCREMENT pour la table `logement`
 --
 ALTER TABLE `logement`
-  MODIFY `id_logement` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT pour la table `reservation`
---
-ALTER TABLE `reservation`
-  MODIFY `id_reservation` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_logement` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `id_utilisateur` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_utilisateur` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
